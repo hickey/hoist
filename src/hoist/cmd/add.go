cd831eac48bb60bae81314fca36c1558ae776356
Gerard Hickey <gerard.hickey@audiencescience.com>
2016-07-24 21:25:04 -0700

    Added viper and more switches

--- src/hoist/cmd/add.go
+++ src/hoist/cmd/add.go
@@ -1,16 +1,31 @@
 package cmd
 
 import (
-	"app"
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-var label string
+/*
+
+  hoist add --env VAR --label LAB=VAL --container FOO --volume DIR=DIR --port PORT=PORT --link MACH=MACH BAR
+
+*/
 
 func init() {
 	RootCmd.AddCommand(addCmd)
-	addCmd.PersistentFlags().StringVarP(&label, "label", "l", "LABEL", "Add a label")
+	addCmd.PersistentFlags().BoolP("label", "l", false, "Add a label")
+	addCmd.PersistentFlags().BoolP("env", "e", false, "Add environmental variable")
+	addCmd.PersistentFlags().BoolP("volume", "V", false, "Add volume mapping")
+	addCmd.PersistentFlags().BoolP("port", "p", false, "Docker port mapping")
+	addCmd.PersistentFlags().BoolP("link", "L", false, "Docker container linkage map")
+
+	viper.BindPFlag("label", addCmd.PersistentFlags().Lookup("label"))
+	viper.BindPFlag("env", addCmd.PersistentFlags().Lookup("env"))
+	viper.BindPFlag("volume", addCmd.PersistentFlags().Lookup("volume"))
+	viper.BindPFlag("port", addCmd.PersistentFlags().Lookup("port"))
+	viper.BindPFlag("link", addCmd.PersistentFlags().Lookup("link"))
+
 }
 
 var addCmd = &cobra.Command{
@@ -23,6 +38,6 @@ var addCmd = &cobra.Command{
 
 func addCommand(args []string) {
 	fmt.Println("add command: ", args)
-	fmt.Println("labels: ", label)
-	fmt.Println("verbose: ", app.GetSettingBool("verbose"))
+	fmt.Println("labels: ", viper.Get("label"))
+	fmt.Println("verbose: ", viper.Get("verbose"))
 }
