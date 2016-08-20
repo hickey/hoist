7ab1e1e51c47490d1227ad23b47e074d3239c55e
Gerard Hickey <gerard.hickey@audiencescience.com>
2016-08-20 15:48:36 -0700

    Added simple DB dump command

---
+++ src/hoist/cmd/dump.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"hoist/store"
+)
+
+func init() {
+	RootCmd.AddCommand(dumpCmd)
+}
+
+var dumpCmd = &cobra.Command{
+	Use:   "dump NAME",
+	Short: "Dump the BoltDB contents for NAME.",
+	Run: func(cmd *cobra.Command, args []string) {
+		dumpCommand(args)
+	},
+}
+
+func dumpCommand(name []string) {
+	store.DumpStartup(name[0])
+}
